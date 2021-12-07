e71b00500753cbe67e54aad321ea8f029de7a94b
EmilDahlen <emildahlen.se@gmail.com>
2021-12-07 21:11:04 +0100

    ┏( ͡▀̿ ̿ ͜ʖ ͡▀̿ ̿ )┛

--- Day_6/problem_1,2.py
+++ Day_6/problem_1,2.py
@@ -1,9 +1,7 @@
 def sim(n, dct):
     for _ in range(n): dct[0], dct[1], dct[2], dct[3], dct[4], dct[5], dct[6], dct[7], dct[8] = dct[1], dct[2], dct[3], dct[4], dct[5], dct[6], dct[7] + dct[0], dct[8], dct[0]
     return sum(dct.values())
-with open(r"C:\Users\emild\OneDrive\Dokument\GitHub\Advent-of-Code-2021\Day_6\input.txt", "r+") as file: file_input = file.read().split(",")
-dct = dict()
+with open(r"C:\Users\emild\OneDrive\Dokument\GitHub\Advent-of-Code-2021\Day_6\input.txt", "r+") as file: file_input = file.read().split(","); dct = dict(); 
 for i in range(9): dct[i] = int()
 for fish in file_input: dct[int(fish)] += 1
-print(sim(80, dct))
-print(sim(256, dct))
+print(f"{sim(80, dct)}\n{sim(256, dct)}")
