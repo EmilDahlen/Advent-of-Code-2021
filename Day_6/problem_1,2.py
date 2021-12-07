404d9dc9923dbd7264455082f0d72d8829889047
EmilDahlen <emildahlen.se@gmail.com>
2021-12-07 21:04:14 +0100

    ┏( ͡▀̿ ̿ ͜ʖ ͡▀̿ ̿ )┛

---
+++ Day_6/problem_1,2.py
@@ -0,0 +1,9 @@
+def sim(n, dct):
+    for _ in range(n):
+        dct[0], dct[1], dct[2], dct[3], dct[4], dct[5], dct[6], dct[7], dct[8] = dct[1], dct[2], dct[3], dct[4], dct[5], dct[6], dct[7] + dct[0], dct[8], dct[0]
+    return sum(dct.values())
+with open(r"C:\Users\emild\OneDrive\Dokument\GitHub\Advent-of-Code-2021\Day_6\input.txt", "r+") as file: file_input = file.read().split(",")
+dct = dict()
+for i in range(9): dct[i] = int()
+for i in file_input: dct[int(i)] += 1
+print(sim(256, dct))
